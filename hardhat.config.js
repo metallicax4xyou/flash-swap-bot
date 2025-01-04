c2a425732943a7e7d6151c047956b03f35826caa
Taylor Marlow <metallicax4xyou@gmail.com>
2025-01-04 14:42:43 +0000

    k

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,27 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
-};
+  solidity: {
+      compilers: [
+            {
+                    version: "0.8.19", // Use a Solidity version compatible with Uniswap V3
+                          },
+                                {
+                                        version: "0.7.6", // Uniswap V3 Periphery uses 0.7.6
+                                              }
+                                                  ],
+                                                    },
+                                                      networks: {
+                                                          hardhat: {
+                                                                forking: {
+                                                                        url: "YOUR_ALCHEMY_URL", // Replace with your Alchemy URL
+                                                                              }
+                                                                                  }
+                                                                                    },
+                                                                                      paths: {
+                                                                                          sources: "./contracts",     // Where your .sol contracts are
+                                                                                              tests: "./test",             // Where your tests are
+                                                                                                  cache: "./cache",           // Cache directory (for compilation artifacts)
+                                                                                                      artifacts: "./artifacts",   // Where to put compiled output
+                                                                                                        }
+                                                                                                        };
